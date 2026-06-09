adfee2dd86843fec3c0a80ad825dddfddbc9123e
InaTix <ina.tix@yandex.ru>
2026-06-09 15:37:50 +0300

    Add new solutions

---
+++ 2_vectors/vector5_more_replication.v
@@ -0,0 +1,11 @@
+module top_module (
+    input a, b, c, d, e,
+    output [24:0] out );//
+	
+    wire [24:0] top_vector;
+    wire [24:0] bottom_vector;
+    assign top_vector = {{5{a}}, {5{b}}, {5{c}}, {5{d}}, {5{e}}};
+    assign bottom_vector = {5{{a, b, c, d, e}}};
+    assign out = top_vector ~^ bottom_vector;
+
+endmodule
